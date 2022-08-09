abf54eb751923583db25102a252cd95158c0ad1c
MurtazaNeher <murtazaradio@gmail.com>
2022-08-09 15:57:29 +0530

    09-08-22

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// const swiperL = document.querySelector('.swiper').swiper;
+
+// $(".header-button-prev").on("click", function(){
+//     console.log("clicked")
+//     swiperL.slidePrev();
+// })
+
+// $(".header-button-next").on("click", function(){
+//     console.log("clicked")
+//     swiperL.slideNext();
+// })
